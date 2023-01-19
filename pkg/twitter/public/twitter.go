aafe93aeec4532afb4adbd040f74fc1bfc275aff
Ayaka Neko <neko@ayaka.moe>
2023-01-19 19:49:18 +0800

    fix: unable to reobtain twitter guest token when it expires

--- pkg/twitter/public/twitter.go
+++ pkg/twitter/public/twitter.go
@@ -13,54 +13,53 @@ import (
 )
 
 type Client struct {
-	reqClient  *req.Client
-	GuestToken string
+	reqClient *req.Client
+
+	guestToken           string
+	guestTokenObtainedAt time.Time
 }
 
 func NewClient() (*Client, error) {
 	c := req.
 		C().
 		SetBaseURL("https://api.twitter.com").
-		// EnableDumpAll().
 		SetUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.52").
 		SetCommonHeader("Referer", "https://twitter.com/").
 		SetCommonHeader("Origin", "https://twitter.com").
 		SetCommonBearerAuthToken(twitter_public_types.GuestBearerToken)
-
 	client := &Client{
 		reqClient: c,
-	}
-
-	_, _, err := lo.AttemptWithDelay(100, time.Second, func(index int, duration time.Duration) error {
-		err := client.GuestActivate()
-		if err != nil {
-			log.Println("failed to activate Twitter guest token, retrying...")
-			return err
-		}
-
-		return nil
-	})
-	if err != nil {
-		return nil, err
 	}
 
 	return client, nil
 }
 
-func (c *Client) GuestActivate() error {
+func (c *Client) ActivateGuest() error {
+	c.reqClient.ClearCookies()
+
 	var guestActivateResp twitter_public_types.GuestActivateResp
 
-	resp, err := c.reqClient.R().
-		SetResult(&guestActivateResp).
-		Post("/1.1/guest/activate.json")
+	_, _, err := lo.AttemptWithDelay(100, time.Second, func(index int, duration time.Duration) error {
+		resp, err := c.reqClient.R().
+			SetResult(&guestActivateResp).
+			Post("/1.1/guest/activate.json")
+		if err != nil {
+			log.Println("failed to activate Twitter guest token, retrying...")
+			return err
+		}
+		if !resp.IsSuccess() {
+			log.Println("failed to activate Twitter guest token, retrying...")
+			return fmt.Errorf("request to %s failed: status code: %d, reason: %v", resp.Request.URL, resp.StatusCode, resp)
+		}
+
+		return nil
+	})
 	if err != nil {
 		return err
 	}
-	if !resp.IsSuccess() {
-		return fmt.Errorf("request to %s failed: status code: %d, reason: %v", resp.Request.URL, resp.StatusCode, resp)
-	}
 
-	c.GuestToken = guestActivateResp.GuestToken
+	c.guestToken = guestActivateResp.GuestToken
+	c.guestTokenObtainedAt = time.Now()
 	return nil
 }
 
@@ -68,6 +67,13 @@ func (c *Client) GuestActivate() error {
 //
 // https://github.com/fa0311/TwitterInternalAPIDocument/blob/master/docs/markdown/GraphQL.md#tweetdetail
 func (c *Client) TweetDetail(tweetID string) (*twitter_public_types.TweetDetailResp, error) {
+	if c.guestTokenObtainedAt.IsZero() || time.Since(c.guestTokenObtainedAt) > 15*time.Minute {
+		err := c.ActivateGuest()
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	newParamVariables := twitter_public_types.DefaultGetTweetDetailParamVariables
 	newParamVariables.FocalTweetID = tweetID
 	newParamVariablesJSON := string(lo.Must(json.Marshal(newParamVariables)))
@@ -79,7 +85,7 @@ func (c *Client) TweetDetail(tweetID string) (*twitter_public_types.TweetDetailR
 	resp, err := c.reqClient.R().
 		SetQueryParam("variables", newParamVariablesJSON).
 		SetQueryParam("features", newParamFeaturesJSON).
-		SetHeader("X-Guest-Token", c.GuestToken).
+		SetHeader("X-Guest-Token", c.guestToken).
 		SetResult(&tweetDetailResp).
 		Get("/graphql/HQ_gjq7zDNvSiJOCSkwUEw/TweetDetail")
 	if err != nil {
